0d95286b0a1aec45d65bc78f31caae6c16c3cbec
ngud-119 <passiondev7@gmail.com>
2021-04-07 13:13:51 +0800

    implement strored proc repository

---
+++ BulkyBook.DataAccess/Repository/IRepository/ISP_Call.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface ISP_Call : IDisposable
+    {
+        T Single<T>(string procedureName, DynamicParameters param = null);
+
+        void Execute(string procedureName, DynamicParameters param = null);
+
+        T OneRecord<T>(string procedureName, DynamicParameters param = null);
+
+        IEnumerable<T> List<T>(string procedureName, DynamicParameters param = null);
+
+        Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedureName, DynamicParameters param = null);
+    }
+}
