18a2f178dcf48b11cfe9dfcf40d652c1e86c38eb
ngud-119 <passiondev7@gmail.com>
2021-04-07 21:17:03 +0800

    category upsert post action

--- BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -10,5 +10,7 @@ namespace BulkyBook.DataAccess.Repository.IRepository
     {
         ICategoryRepository Category { get; }
         ISP_Call SP_Call { get; }
+
+        void Save();
     }
 }
--- BulkyBook/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyBook/Areas/Admin/Controllers/CategoryController.cs
@@ -40,6 +40,25 @@ namespace BulkyBook.Areas.Admin.Controllers
             return View(category);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
 
         #region API CALLS
 
@@ -51,5 +70,6 @@ namespace BulkyBook.Areas.Admin.Controllers
         }
 
         #endregion
+
     }
 }
