8c49660ae6094ea6865a55a3772125e754506db4
ngud-119 <passiondev7@gmail.com>
2021-04-07 15:17:19 +0800

    create category controller

---
+++ BulkyBook/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
