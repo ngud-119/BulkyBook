6d075a37c46d8ffcbd3356db7c8d14e9b7de2d94
ngud-119 <passiondev7@gmail.com>
2021-04-09 15:56:19 +0800

    common error and delete product

--- BulkyBook.Models/Product.cs
+++ BulkyBook.Models/Product.cs
@@ -12,6 +12,7 @@ namespace BulkyBook.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Description { get; set; }
         [Required]
